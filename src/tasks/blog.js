7b00b7cbffd099b356fcb7227f1256831e1122b2
Dirk Holtwick <dirk.holtwick@gmail.com>
2018-02-23 10:09:31 +0100

    Blog implementation

--- src/tasks/blog.js
+++ src/tasks/blog.js
@@ -18,14 +18,124 @@
 // @flow
 // @jsx jsx
 
+import {parseMarkdown, prependXMLIdentifier, setXMLMode} from '../../lib'
+import {dom} from '../dom'
 import {jsx} from '../jsx'
 import {SeaSite} from '../site'
+import dateformat from 'dateformat'
+import _ from 'lodash'
 
-let defaults = {
-    pattern: /\.md/,
-    handler: null,
+function pathToHTMLPath(path) {
+    return path.replace(/\..+?$/, '.html').replace(/\/-/, '/')
 }
 
-export function blog(site: SeaSite, opt: Object = {}) {
+let defaults = {
+    pattern: /blog\/.*\.md$/,
+    handler: null,
+    title: 'Blog',
+    url: '',
+    description: '',
+    copyright: '',
+    author: '',
+    language: 'en-en',
+    template(site) {
+        return <div>
+            <div id="content"></div>
+        </div>
+    },
+}
+
+export function blog(site: SeaSite, opt: Object = {}): Array<Object> {
     opt = Object.assign({}, defaults, opt)
+
+    // Collect blog articles
+    let posts = []
+
+    {
+        site.handle(opt.pattern, (content, path) => {
+            let md = parseMarkdown(content)
+            let props = md.props
+            let html = md.html
+            let {title, date, hidden} = props
+            if (hidden || path.indexOf('/-') > 0) {
+                return
+            }
+            posts.push({
+                html,
+                props,
+                title,
+                date,
+                hidden,
+                path,
+                htmlPath: pathToHTMLPath(path),
+            })
+            return false
+        })
+    }
+
+    // Sort the posts
+    {
+        posts = _.sortBy(posts, 'date').reverse()
+    }
+
+    // RSS
+    {
+        setXMLMode(true)
+        let atomContent = site.handleString(
+            <rss version="2.0" xmlns__atom="http://www.w3.org/2005/Atom">
+                <channel>
+                    <title>{opt.title}</title>
+                    <link>{opt.url}/</link>
+                    <description>{opt.description}</description>
+                    <language>{opt.language}</language>
+                    <copyright>{opt.copyright}</copyright>
+                    <pubDate>{dateformat(new Date(), 'isoDateTime')}</pubDate>
+                </channel>
+            </rss>, {xmlMode: true})
+        for (let post of posts) {
+            let atomEntry =
+                <item>
+                    <title>{post.title}</title>
+                    <link>{site.absoluteURL(post.htmlPath)}</link>
+                    <pubDate>{dateformat(post.date, 'isoDateTime')}</pubDate>
+                    <author>{opt.author}</author>
+                    <description>{post.html}</description>
+                    <guid>{post.htmlPath}</guid>
+                </item>
+            atomContent('channel').append(atomEntry)
+        }
+        setXMLMode(false)
+
+        const xml = prependXMLIdentifier(atomContent.xml())
+        site.write(`/blog/atom.xml`, xml)
+        site.write(`/atom.xml`, xml)
+
+        // Blog Archive
+        {
+            let $ = dom(opt.template(site))
+            if ($) {
+                let group = 'blog'
+                $(`li[data-group="${group}"]`).addClass('active')
+                $('#content').html(
+                    <div>
+                        <h1 className="blog-post-title">Blog</h1>
+                        <ul>
+                            {posts.map(post => <li>
+                                <a href={site.url(pathToHTMLPath(post.path))}>
+                                    {post.title}
+                                </a>
+                                {/*({dateformat(post.date, 'longDate')})*/}
+                            </li>)}
+                        </ul>
+                    </div>,
+                )
+                $('title').text(opt.title)
+                $('#recent-posts-container').remove()
+                site.write('/blog/index.html', $.html())
+            }
+        }
+
+        return posts
+    }
+
 }
