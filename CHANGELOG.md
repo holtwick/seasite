1913fdc1e859c4aa11495dd93ff9204f99aaca9b
Dirk Holtwick <dirk.holtwick@gmail.com>
2018-03-01 12:03:00 +0100

    The new img plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 0.3 (2018-03) 
+
+- plugin.img: Retrieve image sizes, also for @2x and @3x and add corresponding attributes
+
+## 0.2 (2018-02)
+
+
+- Initial release
---
+++ src/plugins/img.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Dirk Holtwick <https://holtwick.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// @flow
+
+import {join} from 'path'
+import {existsSync} from 'fs'
+
+const sizeOf = require('image-size')
+
+const OPT = {}
+
+let cache = {}
+
+function isAbsoluteURL(url: string) {
+    return url.indexOf('http') === 0
+}
+
+export function img(gopt: Object = {}) {
+    gopt = Object.assign({}, OPT, gopt)
+
+    return ($: Function, opt: Object = {}) => {
+        opt = Object.assign({}, gopt, opt)
+
+        const basePath = opt.basePath || opt.site.basePath
+        const path = opt.path || 'index.html'
+
+        if (basePath) {
+            $('img[src]').each((i, el) => {
+                let img = $(el)
+                let src = img.attr('src')
+
+                if (isAbsoluteURL(src)) {
+                    return
+                }
+
+                let p
+                if (src[0] === '/') {
+                    src = src.substr(1)
+                    p = join(basePath, src)
+                } else {
+                    p = join(basePath, path, '..', src)
+                }
+
+                if (!existsSync(p)) {
+                    console.error(`Image at ${p} is referenced in ${path} but is missing!`)
+                    return
+                }
+
+                let size = sizeOf(p);
+
+                console.log('[img]', src, p, size)
+
+                // console.log(dimensions.width, dimensions.height);
+            })
+        }
+    }
+
+}
